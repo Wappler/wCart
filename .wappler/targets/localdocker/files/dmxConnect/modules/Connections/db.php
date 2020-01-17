e60bf8cf39c5b3fda6789ae7c88e025fc7106880
Ben Pleysier <ben@pleysier.com.au>
2020-01-18 09:53:43 +1100

    Beta v1.1

---
+++ .wappler/targets/localdocker/files/dmxConnect/modules/Connections/db.php
@@ -0,0 +1,15 @@
+<?php
+// Database Type : "MySQL"
+// Database Adapter : "mysql"
+$exports = <<<'JSON'
+{
+    "name": "db",
+    "module": "dbconnector",
+    "action": "connect",
+    "options": {
+        "server": "mysql",
+        "connectionString": "mysql:host=db;sslverify=false;port=3306;dbname=wcart;user=db_user;password=tutRmDTf;charset=utf8"
+    }
+}
+JSON;
+?>
