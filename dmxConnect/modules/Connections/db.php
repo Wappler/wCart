ef401c7f5fca9a56a7e3b641ff66a40705b7ab5e
Ben Pleysier <ben@pleysier.com.au>
2020-02-17 17:36:15 +1100

    Beta v2.3 corrections

--- dmxConnect/modules/Connections/db.php
+++ dmxConnect/modules/Connections/db.php
@@ -8,10 +8,7 @@ $exports = <<<'JSON'
     "action": "connect",
     "options": {
         "server": "mysql",
-        "connectionString": "mysql:host=db;sslverify=false;port=3306;dbname=wcart;user=db_user;password=tutRmDTf;charset=utf8",
-        "limit" : 1000,
-        "debug" : false,
-        "meta"  : {"allTables":["categories","company","countries","currencies","customers","metatags","orderdetails","orders","payment_gateway","product_images","products","regions","users"],"allViews":[],"tables":{"countries":{"columns":{"CountryID":{"type":"int","primary":true},"CountryISO":{"type":"char","size":2},"CountryName":{"type":"varchar","size":80},"CountryRegionName":{"type":"char","size":30,"nullable":true},"CountryRegionRequired":{"type":"tinyint","defaultValue":"0"}}},"currencies":{"columns":{"CurrencyID":{"type":"int","primary":true},"CurrencyCountryISO":{"type":"varchar","size":5},"CurrencyISO":{"type":"varchar","size":5,"nullable":true}}}}}
+        "connectionString": "mysql:host=db;sslverify=false;port=3306;dbname=wcart;user=db_user;password=tutRmDTf;charset=utf8"
     }
 }
 JSON;
